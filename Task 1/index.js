24aade8173cc62a6c7e52e16e03cacb4e4358348
bipinrajbhar07 <bipinrajbhar.bscit@gmail.com>
2024-08-24 14:23:55 +0530

    print a teckpathshala output

---
+++ Task 1/index.js
@@ -0,0 +1,51 @@
+var resume={
+    name:"Bipin Rajbhar",
+    emailid:"bipinrajbhar.bscit@gmail.com",
+    mobile:9768368018,
+    city:"vikroli park site,mumbai",
+    education:[
+          {
+            college: "R.J.COLLEGE OF SCIENCE,COMMERCE & ART'S GHATKOPAR WEST",
+            course: "Pursuing msc-it",
+            grade: "7.70 CGPA sem 1",
+          },
+          {
+            college: "PVGCST College Ghatkopar East",
+            course: "HSC",
+            grade: "61.10%",
+            duration: "June 2017 - June 2019",
+          },
+        ],
+      
+        skills: [
+          {
+            programmingLanguages: ["HTML", "CSS", "Javascript"],
+          },
+          {
+            Libraries: ["ReactJs", "ExpressJs", "NodeJs"],
+          },
+        ],
+      
+        projects: [
+          {
+            title: "Ecommerce-Pharmacy Manegment",
+            languagesUsed: ["CSS", "HTML", "JAVA", "NodeJs","mongo db"],
+            description:
+             [ "Successfully built and deployed a MERN stack e-commerce app with authentication , authorization,and CRUD operations.",
+              "Developed both user and admin panels for efficient management of the online store.",
+              "Integrated Stripe payment gateway for secure and seamless transactions.",
+            ],
+
+          },
+        ],
+      
+        coursesCertifications: [
+          {
+            title: "MACHINE LEARNING,R PROGRAMING LANGUAGES",
+            link: "NOT AVAILBLE",
+          },
+          
+        ],
+      };
+      
+      console.log(resume);
